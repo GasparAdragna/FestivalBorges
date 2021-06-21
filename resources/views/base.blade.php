7e07dd837d700510e2e43979d67a4d129036fc11
Gaspar <gaspar.jac@hotmail.com>
2021-06-21 14:25:46 -0300

    agregado ganalytics

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -8,6 +8,14 @@
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
     <link href="/css/custom.css" rel="stylesheet">
     <link href="/css/all.min.css" rel="stylesheet">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-CC0G7GXGP6"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-CC0G7GXGP6');
+    </script>
     @yield('title')
   </head>
   <body>
