5a29d42bef84f2d9a31c0eb44de63c343f48de36
GasparAdragna <gaspar.jac@hotmail.com>
2021-07-06 15:30:37 -0300

    cambio hotjar

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -9,11 +9,11 @@
     <meta name="description" content="Festival para homenajear a uno de los máximos exponentes de la literatura argentina a los 122 años de su nacimiento. Actividades libres y gratuitas">
     <meta name="keywords" content="Festival Borges, Jorge Luis Borges, Borges">
     <meta name="robots" content="index, all, follow">
-    <!-- Hotjar Tracking Code for www.festivalborges.com.ar -->
+    <!-- Hotjar Tracking Code for festivalborges.com.ar -->
     <script>
       (function(h,o,t,j,a,r){
           h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-          h._hjSettings={hjid:2490032,hjsv:6};
+          h._hjSettings={hjid:2490041,hjsv:6};
           a=o.getElementsByTagName('head')[0];
           r=o.createElement('script');r.async=1;
           r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
