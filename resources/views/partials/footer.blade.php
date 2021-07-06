40a25bdd256cc9b8dad523d1e14315e5d881d96e
GasparAdragna <gaspar.jac@hotmail.com>
2021-07-06 12:50:27 -0300

    cambios en el footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer bg-light">
   <div class="container">
     <div class="row">
-      <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
+      <div class="col-lg-4 h-100 text-center text-lg-left my-auto">
         <ul class="list-inline mb-2">
           <li class="list-inline-item">
             <a href="/organizacion">Acerca de</a>
@@ -21,7 +21,10 @@
         </ul>
         <p class="text-muted small mb-4 mb-lg-0">&copy; Festival Borges 2021. Todos los derechos reservados.</p>
       </div>
-      <div class="col-lg-6 h-100 text-center text-lg-right my-auto">
+      <div class="col-lg-4 text-center d-flex align-items-center justify-content-center">
+        <p class="text-muted small mb-4 mb-lg-0">Una página de <a href="https://www.linkedin.com/in/gaspar-adragna-282945142/" target="_blank">Gaspar Adragna</a></p>
+      </div>
+      <div class="col-lg-4 h-100 text-center text-lg-right my-auto">
         <ul class="list-inline mb-0">
           <li class="list-inline-item mr-3">
             <a href="https://www.facebook.com/profile.php?id=100069735954304" target="_blank">
