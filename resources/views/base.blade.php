286168d8ee9e529e1effdda387e9fa695171cf79
GasparAdragna <gaspar.jac@hotmail.com>
2021-07-06 15:07:09 -0300

    metaetiquetas

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -3,6 +3,12 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Festival Borges">
+    <meta name="language" content="spanish">
+    <meta name="audience" content="all">
+    <meta name="description" content="Festival para homenajear a uno de los máximos exponentes de la literatura argentina a los 122 años de su nacimiento. Actividades libres y gratuitas">
+    <meta name="keywords" content="Festival Borges, Jorge Luis Borges, Borges">
+    <meta name="robots" content="index, all, follow">
     <link href="/css/landing-page.css" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
