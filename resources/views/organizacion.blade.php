38be1295fad9e6ab5843290dcda4200e0d0e2893
GasparAdragna <gaspar.jac@hotmail.com>
2021-07-14 21:05:09 -0300

    agregadas imágenes de las organizadoras

--- resources/views/organizacion.blade.php
+++ resources/views/organizacion.blade.php
@@ -10,24 +10,37 @@
       <h2>Festival</h2>
       <br>
       <h4>Dirección General</h4>
-      <div class="pl-2">
-        <h5>Vivian Dragna</h5>
-          <p class="mb-0">Magister en Escritura Creativa (Universidad Tres de Febrero, Argentina)</p>
-          <p class="mb-0">Escritora</p>
-          <p class="mb-0">Publicó la novela <b><i>Los Deseos</i></b> (Alto Pogo). Próximamente publicará la novela <b><i>Prisión Paraíso</i></b> (Corregidor).</p>
-          <p class="mb-0"><a href="https://www.instagram.com/viviandragna/" target="_blank">@viviandragna</a></p>
+      <div class="row pl-2 mt-4">
+        <div class="col-4">
+          <img class="img-fluid rounded-circle mb-3" src="images/vd.jpg" alt="Vivian Dragna">
         </div>
-      <br>
-      <div class="pl-2">
-        <h5>Marisol Alonso</h5>
-        <p class="mb-0">Es editora, guionista y dicta talleres de escritura. Egresada de la Universidad del Cine (FUC) Trabajó como lectora y editora para editoriales nacionales e internacionales.</p>
-        <p class="mb-0">Como gestora cultural trabajó para Bibliotecas de la Ciudad. Coordinó el lanzamiento del Espacio Borges, la Bienal Borges-Kafka, Biblioteca Casa de Lectura, Festival BAN, la delegación de Barcelona como invitada en la Feria del Libro, el encuentro Estrategias de profesionalización en el campo editorial y literario (Buenos Aires-Berlín).</p>
-        <p class="mb-0">Dictó talleres de escritura en Biblioteca Nacional, Museo de Bellas Artes, Casa de la lectura, entre otros. Trabaja en gestión cultural, da talleres de escritura y es editora free lance.</p>
-        <p class="mb-0"><a href="https://www.instagram.com/marisolalonso1/" target="_blank">@marisolalonso1</a></p>
+        <div class="col-6 d-flex align-items-center">
+          <div class="row ">
+            <div class="col-12">
+              <h5>Vivian Dragna</h5>
+              <p class="mb-0">Magister en Escritura Creativa (Universidad Tres de Febrero, Argentina)</p>
+              <p class="mb-0">Escritora</p>
+              <p class="mb-0">Publicó la novela <b><i>Los Deseos</i></b> (Alto Pogo). Próximamente publicará la novela <b><i>Prisión Paraíso</i></b> (Corregidor).</p>
+              <p class="mb-0"><a href="https://www.instagram.com/viviandragna/" target="_blank">@viviandragna</a></p>
+            </div>
+          </div>
+
+        </div>
       </div>
       <br>
-      {{-- <h4>Diseño Web</h4>
-      <div class="pl-2">
-        <h5>Gaspar Adragna</h5>
-      </div> --}}
+      <div class="row">
+        <div class="col-6 d-flex align-items-center">
+          <div class="pl-2">
+            <h5>Marisol Alonso</h5>
+            <p class="mb-0">Es editora, guionista y dicta talleres de escritura. Egresada de la Universidad del Cine (FUC) Trabajó como lectora y editora para editoriales nacionales e internacionales.</p>
+            <p class="mb-0">Como gestora cultural trabajó para Bibliotecas de la Ciudad. Coordinó el lanzamiento del Espacio Borges, la Bienal Borges-Kafka, Biblioteca Casa de Lectura, Festival BAN, la delegación de Barcelona como invitada en la Feria del Libro, el encuentro Estrategias de profesionalización en el campo editorial y literario (Buenos Aires-Berlín).</p>
+            <p class="mb-0">Dictó talleres de escritura en Biblioteca Nacional, Museo de Bellas Artes, Casa de la lectura, entre otros. Trabaja en gestión cultural, da talleres de escritura y es editora free lance.</p>
+            <p class="mb-0"><a href="https://www.instagram.com/marisolalonso1/" target="_blank">@marisolalonso1</a></p>
+          </div>
+        </div>
+        <div class="col-4">
+          <img class="img-fluid rounded-circle mb-3" src="images/ma.jpg" alt="Marisol Alonso">
+        </div>
+      </div>
+      <br>
 @endsection
