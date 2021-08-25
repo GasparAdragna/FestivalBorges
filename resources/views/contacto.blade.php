15eaecd81c66c4860c53ed9bb814a6593d644a18
GasparAdragna <gaspar.jac@hotmail.com>
2021-08-25 16:47:55 -0300

    ayuda en formulario de contacto

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -31,8 +31,10 @@
                     </div>
                 @endif
                 <br>
+                <h4>Las charlas y talleres NO se publicarán en YouTube una vez finalizada la actividad por el momento. Si te perdiste una actividad o querés reverla, seguinos en Instagram para enterarte cuando publicamos los videos. </h4>
+                <h4>Si no recibió el link a la charla, lo más probable es que esté en su casilla SPAM. La noche anterior enviamos todos los correos a los inscriptos</h4>
                 <p>Las actividades son <b>gratuitas</b> y con inscripcion previa. Para inscribirse, visite las actividades <a href="/pordia" target="_blank">por día</a> y haga click en "Quiero registrarme" en la actividad deseada</p>
-                <form class="row" method="post">
+                <form class="row" method="post" id="contactForm">
                     @csrf
                     <div class="col-12 mb-3">
                         <label for="name" class="form-label">Nombre completo</label>
@@ -64,4 +66,28 @@
 
 @section('js')
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        document.getElementById('contactForm').addEventListener('submit', function(){
+            window.event.preventDefault();
+            Swal.fire({
+                title: 'Por favor lea lo siguiente:',
+                html:
+                '<p> Las actividades pasadas <b>no se pueden rever</b> por el momento. </p>' +
+                '<p> Si no le llegó el link a la charla seguramente está en su casilla de SPAM o No Deseado. </p>' +
+                '<p> Enviamos los correos con los links la noche anterior a todos los inscriptos. </p>' + 
+                '<p> Si no le funciona el botón del mail, copie y pegue la dirección en su navegador </p>',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Enviar',
+                cancelButtonText: 'Cancelar'
+                }).then((result) => {
+                if (result.isConfirmed) {
+                    document.getElementById('contactForm').submit()
+                }
+            })
+        });
+    </script>
 @endsection
